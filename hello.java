42b63480415c956af8040a68b00f5bf2c60b669e
=khaled <k.hamoud@live.com>
2023-08-16 11:45:34 +0200

    Add documentation comments to Hello class

--- hello.java
+++ hello.java
@@ -1,14 +1,26 @@
+/**
+ * This class represents a person with first name, last name, and age.
+ */
 public class Hello {
     private String vorname;
     private String nachname;
     private int alter;
 
+    /**
+     * Constructs a new Hello instance.
+     * @param vorname The first name of the person.
+     * @param nachname The last name of the person.
+     * @param alter The age of the person.
+     */
     public Hello(String vorname, String nachname, int alter) {
         this.vorname = vorname;
         this.nachname = nachname;
         this.alter = alter;
     }
 
+    /**
+     * Prints the information about the person.
+     */
     public void printInformation() {
         System.out.println("Vorname: " + vorname);
         System.out.println("Nachname: " + nachname);
